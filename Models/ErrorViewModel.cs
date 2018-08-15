6398f1122aaec4b425124ff9dfc32febd4b90124
ikadom <maee0803@gmail.com>
2018-08-15 20:00:04 +0900

    new

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureSignalrTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
