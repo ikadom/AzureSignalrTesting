35629abccbbd7a204d71a3a0e94907d3b44bc2f3
ikadom <maee0803@gmail.com>
2018-08-19 17:29:05 +0900

    new3

--- Startup.cs
+++ Startup.cs
@@ -31,7 +31,7 @@ namespace AzureSignalrTesting
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseMvc();
+            //app.UseMvc();
             app.UseFileServer();
             app.UseAzureSignalR(routes =>
             {
