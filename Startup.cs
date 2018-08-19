a046de44d395d49cdcafdd747f76081f2da5bb2c
ikadom <maee0803@gmail.com>
2018-08-19 18:02:01 +0900

    認証追加

--- Startup.cs
+++ Startup.cs
@@ -7,10 +7,19 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Newtonsoft.Json.Linq;
+
 namespace AzureSignalrTesting
 {
     public class Startup
     {
+        private const string GitHubClientId = "GitHubClientId";
+        private const string GitHubClientSecret = "GitHubClientSecret";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,7 +34,18 @@ namespace AzureSignalrTesting
             services.AddMvc();
             services.AddSignalR().AddAzureSignalR();
 
-
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie()
+            .AddGitHub(options =>
+            {
+                options.ClientId = Configuration[GitHubClientId];
+                options.ClientSecret = Configuration[GitHubClientSecret];
+                options.Scope.Add("user:email");
+                options.Events = new OAuthEvents
+                {
+                    OnCreatingTicket = GetUserCompanyInfoAsync
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +58,7 @@ namespace AzureSignalrTesting
                 routes.MapHub<Chat>("/chat");
             });
 
+            app.UseAuthentication();
             /*
             if (env.IsDevelopment())
             {
@@ -57,6 +78,26 @@ namespace AzureSignalrTesting
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
             */
+        }
+        private static async Task GetUserCompanyInfoAsync(OAuthCreatingTicketContext context)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+            var response = await context.Backchannel.SendAsync(request,
+                HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+
+            var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+            if (user.ContainsKey("company"))
+            {
+                var company = user["company"].ToString();
+                var companyIdentity = new ClaimsIdentity(new[]
+                {
+                    new Claim("Company", company)
+                });
+                context.Principal.AddIdentity(companyIdentity);
+            }
         }
 /*
         public void ConfigureServices(IServiceCollection services)
